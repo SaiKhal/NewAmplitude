81e7731f812846260b9062b2dda9c8da5c7ac508
Masai Young <masai.young@gmail.com>
2023-01-19 12:18:56 -0500

    fixup! Resolve packages

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SegmentAmplitude",
+    name: "NewAmplitude",
     platforms: [
         .iOS("13.0"),
         .tvOS("11.0"),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SegmentAmplitude",
-            targets: ["SegmentAmplitude"]),
+            name: "NewAmplitude",
+            targets: ["NewAmplitude"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -29,7 +29,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SegmentAmplitude",
+            name: "NewAmplitude",
             dependencies: ["SegmentDeprecated"]),
         
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
