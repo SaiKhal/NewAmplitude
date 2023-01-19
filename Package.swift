e2885ba5e14ef3889332f603070051b1e546cb6e
Masai Young <masai.young@gmail.com>
2023-01-19 12:02:19 -0500

    fixup! Rename amplitude sdk to avoid naming collisions with our new segment sdk

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SegmentAmplitude",
-            dependencies: ["Segment"]),
+            dependencies: ["SegmentDeprecated"]),
         
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
     ]
